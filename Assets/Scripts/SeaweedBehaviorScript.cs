d5a3108cbe57a811b6aad7c78cdc9182055f0216
AustrianBasementDaddy <wardenoath@hotmail.com>
2019-10-02 14:33:15 +0200

    Seaweed ragdoll
    The ragdoll for the seaweed has been rigged.

---
+++ Assets/Scripts/SeaweedBehaviorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaweedBehaviorScript : MonoBehaviour
+{
+    #region Global variables
+    [SerializeField] private List<Rigidbody> rigidbodies;
+    #endregion
+
+
+    void Start()
+    {
+        Setup();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Sets everything up for hot seaweed action.
+    /// </summary>
+    private void Setup ()
+    {
+        Rigidbody rb = GetComponentInChildren<Rigidbody>();
+
+        // Gets all of the rigidbodies. 
+        while (true)
+        {
+            rigidbodies.Add(rb);
+
+            if (rb.GetComponentInChildren<Rigidbody>())
+                rb = rb.GetComponentInChildren<Rigidbody>();
+            else
+                break;
+        }
+
+    }
+}
