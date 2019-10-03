49749f775132d7775e75b80198c61be9d081a0a4
Sunshine-kun <55584033+Sunshine-kun@users.noreply.github.com>
2019-10-03 14:21:55 +0200

    Plast
    Vad Är Plast?

---
+++ Assets/Scripts/FallingController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingController : MonoBehaviour
+{
+	[SerializeField] private float rmin, rmax, zmin, zmax, time, timeR;
+
+	private float rand, rand2, checkValue;
+
+	bool bottom;
+
+	private Rigidbody rb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		if (rmin == 0)
+		{
+			rmin = -5;
+		}
+
+		if (rmax == 0)
+		{
+			rmax = 5;
+		}
+
+		time = timeR;
+		//rand = transform.position.x;
+		//checkValue = rand;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		ClampMove();
+		CountDown();
+	}
+
+	void ClampMove()
+	{
+		if (!bottom)
+		{
+			rand = Random.Range(rmin, rmax);
+			rand2 = Random.Range(zmin, zmax);
+		}
+	}
+
+	void CountDown()
+	{
+		time = time - Time.deltaTime;
+
+		if (time <= 0)
+		{
+			Move();
+			time = timeR;
+		}
+	}
+
+	void Move ()
+	{
+		if (!bottom)
+		rb.velocity = new Vector3(rand, 0, rand2);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		bottom = true;
+	}
+}
