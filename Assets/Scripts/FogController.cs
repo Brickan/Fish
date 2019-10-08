6117e2f58b8ab8f07c6f63945b3819f6c4a18fa4
Brickan <55539411+Brickan@users.noreply.github.com>
2019-10-08 13:31:19 +0200

    CorallFade och Fogcontroller
    fixade så corallen fadear övertiden (får skjustera senare)
    och en distance räknare som påverkar dimman i spelet
    
    Jag är bäst *dab* *dab* *dab*

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FogController : MonoBehaviour
+{
+    private GameObject player;
+    private float startEndDistance;
+    [SerializeField]
+    private float fogMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = new Color(0.19f, 0.3f, 0.47f);
+        player = GameObject.FindGameObjectWithTag("Player");
+        startEndDistance = RenderSettings.fogEndDistance;
+        fogMultiplier = (fogMultiplier == 0) ? fogMultiplier = 3 : fogMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FogDenser();
+    }
+
+    void FogDenser()
+    {
+        RaycastHit hit;
+        Physics.Linecast(transform.position, player.transform.position, out hit);
+        Debug.Log(hit.distance);
+
+        RenderSettings.fogEndDistance = FogCalc(hit.distance);
+
+        Debug.DrawLine(transform.position, player.transform.position, Color.magenta);
+    }
+
+    float FogCalc(float inValue)
+    {
+        float maxEndDist = startEndDistance - (inValue * fogMultiplier);
+
+        if(maxEndDist < 1)
+        {
+            maxEndDist = 1;
+        }
+
+        return maxEndDist;
+    }
+}
