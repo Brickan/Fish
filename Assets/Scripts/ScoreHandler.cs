58febb97fd673f099f78da81d82a2957d9516b97
AustrianBasementDaddy <47384775+AustrianBasementDaddy@users.noreply.github.com>
2019-10-09 14:46:11 +0200

    ScoreHandler and difficulty function
    We now have an incremental score system set up and a difficulty algorithm to go with it. The script is called ScoreHandler.

---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    private static float score = 0.0f;
+
+    [Tooltip("The rate at which the difficulty will incrementally increase.")]
+    [Range(1.1f, 2.0f)]
+    [SerializeField]
+    private float difficulty = 1.1f;
+    private static float sDifficulty = 0.0f;
+
+    [Tooltip("Decide the start value of the 'C' constant in the exponentially increasing spawn rate of plastic.")]
+    [SerializeField]
+    private float startValue = 1.0f;
+    private static float sStartValue = 0.0f;
+
+    private static float plasticSpawnRate = 0.0f;
+
+    void Start()
+    {
+        sDifficulty = difficulty;
+        sStartValue = startValue;
+    }
+
+
+    void Update()
+    {
+        UpdateScore();
+
+        Debug.Log("Score: " + score + " Spawn rate: " + GetPlasticSpawnRate());
+    }
+
+    private void UpdateScore ()
+    {
+        score += Time.deltaTime;
+    }
+
+
+    /// <summary>
+    /// Returns the current score.
+    /// </summary>
+    /// <returns></returns>
+    public static float GetScore ()
+    {
+        return score;
+    }
+
+
+    /// <summary>
+    /// Retuns the current plastic spawn rate.
+    /// </summary>
+    /// <returns></returns>
+    public static float GetPlasticSpawnRate ()
+    {
+        // f(x) = C * a^x
+        // f(x) = plasticSpawnRate
+        // C    = start value (1 by default)
+        // a    = difficulty
+        // x    = score
+
+        plasticSpawnRate = sStartValue * Mathf.Pow(sDifficulty, score);
+        return plasticSpawnRate;
+    }
+
+}
